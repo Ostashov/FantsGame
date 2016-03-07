a0ad28a704b55d288726dcd549570c3a663e7ca9
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-03-07 14:00:46 +0300

    little changes in model.js

--- js/controller.js
+++ js/controller.js
@@ -1,13 +1,5 @@
 jQuery(document).ready(function(){
 	$("#addPlayerButton").click(function() {
-		var src = $("#addPlayerForm").serialize();
-		$.ajax({
-			url: "addPlayer.php",
-			type: "POST",
-			data: src,
-			success: function() {
-				$("#addPlayerResult").html("Игрок успешно добавлен");
-			}
-		});
+		addPlayerIntoDB();
 	});
 });
--- js/model.js
+++ js/model.js
@@ -1,3 +1,11 @@
-jQuery(document).ready(function(){
-
-});
+function addPlayerIntoDB(){
+	var src = $("#addPlayerForm").serialize();
+	$.ajax({
+		url: "addPlayer.php",
+		type: "POST",
+		data: src,
+		success: function() {
+			$("#addPlayerResult").html("Player added");
+		}
+	});
+}
