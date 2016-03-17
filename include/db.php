c1aa05358caf101852f517b62f8b9469336c332c
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-03-17 23:43:01 +0300

    Добавлен charset в db.php #25

--- include/db.php
+++ include/db.php
@@ -11,7 +11,8 @@
 $dsn = "mysql:host=localhost;dbname=fantsDB";
 $opt = array(
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 );
 $pdo = new PDO($dsn, "root", "root", $opt);
 
