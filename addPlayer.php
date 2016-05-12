9c3a7b225084ccdbdb2d1932a1f3d317a8e3f033
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-05-12 21:12:20 +0300

    isMale

--- addPlayer.php
+++ addPlayer.php
@@ -2,9 +2,9 @@
 include("include/db.php");
 
 $playerName = $_REQUEST['playerName'];
-$playerGender = $_REQUEST['playerGender'];
+$playerIsMale = $_REQUEST['playerIsMale'];
 
-$sql= "INSERT INTO players(player_name, player_gender) VALUES ('$playerName', '$playerGender') ";
+$sql= "INSERT INTO players(player_name, player_isMale) VALUES ('$playerName', '$playerIsMale') ";
 //$r=mysql_query ($sql);
 
 $stmt = $pdo->query($sql);
