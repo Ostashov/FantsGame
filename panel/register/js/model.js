50b4936afb85c8e207ca46e3c354864df49a0f63
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-05-13 22:24:53 +0300

    Fix #28 Fix #31

--- panel/register/js/model.js
+++ panel/register/js/model.js
@@ -1,3 +1,13 @@
 function register(userEmail, userPass) {
     alert(userEmail + " " + userPass);
+
+	var src = "email=" + userEmail + "&password=" + userPass;
+	$.ajax({
+		url: "reg.php",
+		type: "POST",
+		data: src,
+		success: function(data) {
+			alert(data);
+		}
+	});
 }
---
+++ panel/register/reg.php
@@ -0,0 +1,18 @@
+<?php
+include("../../include/db.php");
+
+$user_email = $_POST['email'];
+$user_pass = md5($_POST['password']);
+
+$stmt = $pdo->query('SELECT COUNT(0) AS ROW_COUNT FROM users WHERE user_email="'.$user_email.'" ');
+$number = $stmt->fetch(PDO::FETCH_NUM);
+
+if ($number[0] != 0) {
+    echo "exists";
+} else {
+    $sql= "INSERT INTO users(user_email, user_password) VALUES ('$user_email', '$user_pass') ";
+
+    $stmt = $pdo->query($sql);
+    echo "added";
+}
+?>
