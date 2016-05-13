547b10696d92d3422fa6e6a2a83251ea29bb2d4b
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-05-13 22:55:18 +0300

    resultHandler in registration

--- panel/register/js/model.js
+++ panel/register/js/model.js
@@ -1,13 +1,29 @@
 function register(userEmail, userPass) {
     alert(userEmail + " " + userPass);
 
-	var src = "email=" + userEmail + "&password=" + userPass;
-	$.ajax({
-		url: "reg.php",
-		type: "POST",
-		data: src,
-		success: function(data) {
-			alert(data);
-		}
-	});
+    var src = "email=" + userEmail + "&password=" + userPass;
+    $.ajax({
+        url: "reg.php",
+        type: "POST",
+        data: src,
+        success: function(data) {
+            resultHandler(data);
+        }
+    });
+}
+
+function resultHandler(data) {
+    switch (data) {
+        case "exists":
+            printMessage($("#userEmailMessage"), "E-mail занят");
+            printMessage($("#userPasswordMessage"), "");
+            printMessage($("#userPassword2Message"), "");
+            $("#userPassword").val('');
+            $("#userPassword2").val('');
+            break;
+        case "added":
+            break;
+        default:
+            break;
+    }
 }
